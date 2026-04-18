33fa0fa593938f8373f5e5bbcc8655f878c58b85
Felix Weuthen <felixweuth@gmail.com>
2026-04-18 18:29:14 +0200

    remove duplicates

--- packages/dart_edge_runtime/lib/src/contracts/http/http_method.dart
+++
@@ -1,2 +0,0 @@
-/// Supported HTTP methods for [RouteContract]s.
-enum HttpMethod { get, post, put, patch, delete, head, options }
--- packages/dart_edge_runtime/lib/src/contracts/http/json_encodable.dart
+++
@@ -1,6 +0,0 @@
-/// Contract for values that know how to serialize themselves as JSON-friendly
-/// Dart objects.
-abstract interface class JsonEncodable {
-  /// Converts this value into a JSON-friendly representation.
-  Object? toJson();
-}
